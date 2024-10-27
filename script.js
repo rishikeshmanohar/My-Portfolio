21e28922ed9fb0877aabe96b33c4c4d4c2610a59
rishikeshmanohar <rishikeshmanohar17@gmail.com>
2024-10-27 03:41:40 -0700

    Add logger middleware

--- script.js
+++ script.js
@@ -5,6 +5,16 @@
 //     icon.classList.toggle("open");
 // }
 
+const loggerMiddleware = require('./Middleware/loggerMiddleware').default;
+
+const express = require('express');
+const app = express();
+
+// Use the logger middleware
+app.use(loggerMiddleware);
+
+// above code is added for middleware assignment
+
 function toggleMenu(menuSelector = ".menu-links", iconSelector = ".hamburger-icon") {
     const menu = document.querySelector(menuSelector);
     const icon = document.querySelector(iconSelector);
