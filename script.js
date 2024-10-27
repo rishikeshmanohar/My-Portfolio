3b672e0ff577496a8e3d1488e60fbc679bb5be24
rishikeshmanohar <rishikeshmanohar17@gmail.com>
2024-10-27 02:04:07 -0700

    Made arguments reusable for menu

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+// function toggleMenu(){
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
+
+function toggleMenu(menuSelector = ".menu-links", iconSelector = ".hamburger-icon") {
+    const menu = document.querySelector(menuSelector);
+    const icon = document.querySelector(iconSelector);
+    if (menu && icon) {
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+    }
 }
